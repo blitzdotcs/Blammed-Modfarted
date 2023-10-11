f1da62b2842e56a9e0d72a28ebabe6bba613a5c1
Blitz <93739827+blitzexlmao@users.noreply.github.com>
2023-10-11 11:35:22 -0400

    win sprite cam hud (switching to fx engine now)

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -703,6 +703,7 @@ class PlayState extends MusicBeatState
 		iconP2.y = healthBar.y - (iconP2.height / 2);
 		add(iconP2);
 
+		WinSpriteLol.cameras = [camHUD];
 		strumLineNotes.cameras = [camHUD];
 		notes.cameras = [camHUD];
 		healthBar.cameras = [camHUD];
